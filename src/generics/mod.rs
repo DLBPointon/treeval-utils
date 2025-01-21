dbf7a136a86f9b460725f9fdfa477de1f043014a
DLBPointon <damonlbp@hotmail.co.uk>
2025-01-21 17:01:50 +0000

    Updates to header parsing

--- src/generics/mod.rs
+++ src/generics/mod.rs
@@ -107,19 +107,32 @@ fn get_ensemble_symbol(header: String) -> Result<String, Box<dyn std::error::Err
     // static MASTER_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?<prot_id>ENS\w+P\d+.[0-9*])|(?<gene_id>ENS\w+G\d+.[0-9*])|(?<trans_id>ENS\w+T\d+.[0-9*])").unwrap());
     //
 
+    // Alternate Case...
+    // >AT5G16970.1 pep chromosome:TAIR10:5:5575973:5578086:-1 gene:AT5G16970 transcript:AT5G16970.1 gene_biotype:protein_coding transcript_biotype:protein_coding gene_symbol:AER description:alkenal reductase [Source:NCBI gene (formerly Entrezgene);Acc:831560]
+    // Quite different
+
     // Turning the REGEX compilations into Lazy statics turned testing on a 35Mb genome
     // from 16 to 5 minutes!!!
     // This is because this function is used inside a loop
     // so the regex was being compiled every loop
     // Lazy ensures they are compiled only_once (name of the crate... get it)
     // This is still a stupid amount of time but an 11 minute speed up is amazing
-    static RE_1: Lazy<Regex> = Lazy::new(|| Regex::new(r"ENS\w+G\d+.[0-9*]").unwrap());
-    let ens_gene_capture = RE_1
+    static RE_1A: Lazy<Regex> = Lazy::new(|| Regex::new(r"ENS\w+G\d+.[0-9*]").unwrap());
+    static RE_1B: Lazy<Regex> = Lazy::new(|| Regex::new(r"gene:(?<gene_id>\S.*?)\s").unwrap());
+    let ens_gene_capture = RE_1A
         .captures(&header)
         .and_then(|caps| caps.get(0))
         .map_or("NO_GENE_NAME", |m| m.as_str());
     let final_gene_id = if ens_gene_capture == "NO_GENE_NAME" {
-        "".to_string()
+        let ens_gene_backup: String = match RE_1B.captures(&header) {
+            Some(i) => format!("gene_id={};", i.name("gene_id").unwrap().as_str()),
+            None => "".to_string(),
+        };
+        if ens_gene_backup == "NO_GENE_NAME" {
+            "".to_string()
+        } else {
+            ens_gene_backup
+        }
     } else {
         format!("gene_id={};", ens_gene_capture)
     };
@@ -136,23 +149,45 @@ fn get_ensemble_symbol(header: String) -> Result<String, Box<dyn std::error::Err
     };
 
     static RE_3: Lazy<Regex> = Lazy::new(|| Regex::new(r"ENS\w+T\d+.[0-9*]").unwrap());
-    let ens_tran_capture = RE_3
+    static RE_3B: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"transcript:(?<trans_id>\S.*?)\s").unwrap());
+
+    let ens_trans_capture = RE_3
         .captures(&header)
         .and_then(|caps| caps.get(0))
         .map_or("NO_TRAN_NAME", |m| m.as_str());
-    let final_tran_name = if ens_tran_capture == "NO_TRAN_NAME" {
-        "".to_string()
+    let final_trans_name = if ens_trans_capture == "NO_TRAN_NAME" {
+        let ens_trans_backup: String = match RE_3B.captures(&header) {
+            Some(i) => format!("transcript_id={};", i.name("trans_id").unwrap().as_str()),
+            None => "".to_string(),
+        };
+        if ens_trans_backup == "NO_TRANS_NAME" {
+            "".to_string()
+        } else {
+            ens_trans_backup
+        }
     } else {
-        format!("transcript_id={};", ens_tran_capture)
+        format!("transcript_id={};", ens_trans_capture)
     };
 
     let gene_name: Vec<&str> = header.split('|').collect();
-    let final_gname = gene_name[gene_name.len() - 2];
-    let final_gene_name = format!("gene_name={}", final_gname);
 
+    let final_gene_name = if gene_name.len() > 3 {
+        let final_gname = gene_name[gene_name.len() - 2];
+
+        format!("gene_name={}", final_gname)
+    } else {
+        "".to_string()
+    };
+
+    let head = format!(
+        "{}{}{}{}",
+        final_prot_name, final_gene_id, final_trans_name, final_gene_name,
+    );
+    println!("{}", head);
     Ok(format!(
         "{}{}{}{}",
-        final_prot_name, final_gene_id, final_tran_name, final_gene_name,
+        final_prot_name, final_gene_id, final_trans_name, final_gene_name,
     ))
 }
 
